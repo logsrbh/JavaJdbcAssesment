de19cd21324c06e9ea2c548866f505b6e2cf67ac
logsrbh <logsrbh@gmail.com>
2019-11-25 13:48:33 +0530

    JavaJdbcAssesment

---
+++ JavaAssesment/JavaAssesment/src/com/ustglobal/contact/dto/ContactBean.java
@@ -0,0 +1,38 @@
+package com.ustglobal.contact.dto;
+
+public class ContactBean {
+	
+	
+	private String name;
+	
+	private int num;
+	
+	private String groupName;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	
+	
+
+}
